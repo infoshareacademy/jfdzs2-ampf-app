0442953aa7e3f218cedc2d4621adb02431f2a91d
Filip <filip.burakowski@gmail.com>
2018-07-15 17:13:26 +0200

    Fixed code format.

--- src/routes/Recipes/Recipes.js
+++ src/routes/Recipes/Recipes.js
@@ -9,43 +9,45 @@ class Recipes extends PureComponent {
         foundRecipes: [],
     };
 
-    componentDidMount(){
+    componentDidMount() {
         const joinedIngredients = this.props.match.params.ingredients;
-        const quantityOfFoundRecipes = 6;
+        const quantityOfFoundRecipes = 4;
         fetch(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=${joinedIngredients}&limitLicense=false&number=${quantityOfFoundRecipes}&ranking=1`,
-            {headers: {
-                'X-Mashape-Key':'HLjFUpQGdUmshWEfsprU3wV6pi9Ap1ccBUvjsntswHq0ADcQun',
-                'Accept':'application/json'
-            }}
+            {
+                headers: {
+                    'X-Mashape-Key': 'HLjFUpQGdUmshWEfsprU3wV6pi9Ap1ccBUvjsntswHq0ADcQun',
+                    'Accept': 'application/json'
+                }
+            }
         )
             .then(response => response.json())
             .then(data => this.setState({
                 foundRecipes: data,
             }))
-            .catch(error=> console.log(error)
+            .catch(error => console.log(error)
             )
-
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
         console.log(
             this.state
         )
     }
+
     renderRecipes = () =>
-        this.state.foundRecipes.map((item)=>
+        this.state.foundRecipes.map((item) =>
             <Col xs={12} sm={6} lg={4} className='foundRecipe'
                  key={item.id}>
 
-                    <h2 className="foundRecipeTitle">{item.title}</h2>
-                <Link to={'/SingleRecipe/'+item.id}>
+                <h2 className="foundRecipeTitle">{item.title}</h2>
+                <Link to={'/SingleRecipe/' + item.id}>
                     <Image
-                    src={item.image}
-                    alt='Recipe image'
-                    className='foundRecipe-image'
-                    rounded
-                    thumbnail
-                    responsive
+                        src={item.image}
+                        alt='Recipe image'
+                        className='foundRecipe-image'
+                        rounded
+                        thumbnail
+                        responsive
                     />
                 </Link>
             </Col>
